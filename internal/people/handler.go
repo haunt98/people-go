609cd61a85c0f72da65a125f5cca738eb54b8004
Hau Nguyen <hauvipapro@gmail.com>
2022-12-24 22:29:38 +0700

    feat: add remove spaces

--- internal/people/handler.go
+++ internal/people/handler.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/make-go-great/date-go"
 	"github.com/make-go-great/ioe-go"
@@ -51,41 +52,41 @@ func (h *handler) Add(ctx context.Context) error {
 	person.Name = ioe.ReadInput()
 
 	fmt.Printf("Input birthday (example %s): ", date.SupportDateFormats())
-	person.Birthday = ioe.ReadInputEmpty()
+	person.Birthday = removeSpaces(ioe.ReadInputEmpty())
 
 	// TODO: check valid phone
 	fmt.Printf("Input phone: ")
-	person.Phone = ioe.ReadInputEmpty()
+	person.Phone = removeSpaces(ioe.ReadInputEmpty())
 
 	fmt.Printf("Input University: ")
-	person.University = ioe.ReadInputEmpty()
+	person.University = removeSpaces(ioe.ReadInputEmpty())
 
 	fmt.Printf("Input VNCMND: ")
-	person.VNCMND = ioe.ReadInputEmpty()
+	person.VNCMND = removeSpaces(ioe.ReadInputEmpty())
 
 	fmt.Printf("Input VNCCCD: ")
-	person.VNCCCD = ioe.ReadInputEmpty()
+	person.VNCCCD = removeSpaces(ioe.ReadInputEmpty())
 
 	fmt.Printf("Input VNMST: ")
-	person.VNMST = ioe.ReadInputEmpty()
+	person.VNMST = removeSpaces(ioe.ReadInputEmpty())
 
 	fmt.Printf("Input VNBHXH: ")
-	person.VNBHXH = ioe.ReadInputEmpty()
+	person.VNBHXH = removeSpaces(ioe.ReadInputEmpty())
 
 	fmt.Printf("Input CompanyVNG: ")
-	person.CompanyVNG = ioe.ReadInputEmpty()
+	person.CompanyVNG = removeSpaces(ioe.ReadInputEmpty())
 
 	fmt.Printf("Input SocialFacebook: ")
-	person.SocialFacebook = ioe.ReadInputEmpty()
+	person.SocialFacebook = removeSpaces(ioe.ReadInputEmpty())
 
 	fmt.Printf("Input SocialInstagram: ")
-	person.SocialInstagram = ioe.ReadInputEmpty()
+	person.SocialInstagram = removeSpaces(ioe.ReadInputEmpty())
 
 	fmt.Printf("Input SocialTiktok: ")
-	person.SocialTiktok = ioe.ReadInputEmpty()
+	person.SocialTiktok = removeSpaces(ioe.ReadInputEmpty())
 
 	fmt.Printf("Input SocialLinkedin: ")
-	person.SocialLinkedin = ioe.ReadInputEmpty()
+	person.SocialLinkedin = removeSpaces(ioe.ReadInputEmpty())
 
 	return h.service.Add(ctx, &person)
 }
@@ -103,73 +104,73 @@ func (h *handler) Update(ctx context.Context) error {
 	var val string
 
 	fmt.Printf("Update name, current is [%s]: ", person.Name)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.Name = val
 	}
 
 	fmt.Printf("Update birthday, current is [%s]: ", person.Birthday)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.Birthday = val
 	}
 
 	fmt.Printf("Update phone, current is [%s]: ", person.Phone)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.Phone = val
 	}
 
 	fmt.Printf("Input University, current is [%s]: ", person.University)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.University = val
 	}
 
 	fmt.Printf("Input VNCMND, current is: [%s]", person.VNCMND)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.VNCMND = val
 	}
 
 	fmt.Printf("Input VNMST, current is [%s]: ", person.VNMST)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.VNMST = val
 	}
 
 	fmt.Printf("Input VNBHXH, current is [%s]: ", person.VNBHXH)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.VNBHXH = val
 	}
 
 	fmt.Printf("Input CompanyVNG, current is [%s]: ", person.CompanyVNG)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.CompanyVNG = val
 	}
 
 	fmt.Printf("Input SocialFacebook, current is [%s]: ", person.SocialFacebook)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.SocialFacebook = val
 	}
 
 	fmt.Printf("Input SocialInstagram, current is [%s]: ", person.SocialInstagram)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.SocialInstagram = val
 	}
 
 	fmt.Printf("Input SocialTiktok, current is [%s]: ", person.SocialTiktok)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.SocialTiktok = val
 	}
 
 	fmt.Printf("Input SocialLinkedin, current is [%s]: ", person.SocialLinkedin)
-	val = ioe.ReadInputEmpty()
+	val = removeSpaces(ioe.ReadInputEmpty())
 	if val != "" {
 		person.SocialLinkedin = val
 	}
@@ -234,4 +235,9 @@ func (h *handler) Import(ctx context.Context) error {
 
 func (h *handler) Drop(ctx context.Context) error {
 	return h.service.Drop(ctx)
+}
+
+// https://stackoverflow.com/a/42251527
+func removeSpaces(s string) string {
+	return strings.Join(strings.Fields(s), " ")
 }
